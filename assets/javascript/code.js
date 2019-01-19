3e0616341bd4b86a2de08651fd6b7821d38a9b60
Dan <danmorales209@gmail.com>
2019-01-19 11:27:34 -0800

    add some js and JSON files

---
+++ assets/javascript/code.js
@@ -0,0 +1,12 @@
+
+function buildQuestions(emptyArray) {
+    const NUMBER_OF_QUESTIONS = 5;
+    
+    for(let i = 0; i < NUMBER_OF_QUESTIONS; i++) {
+        
+    }
+}
+
+$(document).ready(function () {
+    var questions
+})
